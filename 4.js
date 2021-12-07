d3f1cec1ed5817b92116b79a820c0ae9c0c81587
Екатерина Новоторцева <Gomozova.ekaterin@gmail.com>
2021-12-07 19:59:35 +0300

    4

---
+++ 4.js
@@ -0,0 +1,12 @@
+function day(num)
+{
+    if(num == 1) 
+    return"Понедельник";
+    else if(num == 2)
+    return"Вторник";
+    else if(num == 3);
+    return"Среда";
+}
+
+let c = day(1);
+alert(c);
